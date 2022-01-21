2c34c1d5bb605fb706690537a9b237ea66b88a6c
Simone Anthony <simone.anthony1@yahoo.com>
2022-01-20 22:27:24 -0500

    Usser Schema created

--- models/User.js
+++ models/User.js
@@ -1,11 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
+const validator = require("validator");
 
 const userSchema = new Schema({
-  text: String,
-  username: String,
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  validate: {
+    validator: function (v) {
+      return validator.isEmail(v);
+    },
+    message: "Please enter a valid email",
+  },
+  thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
+  friends: [{ type: Schema.Types.ObjectId, ref: "Friends" }],
 });
 
-const User = model('user', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
