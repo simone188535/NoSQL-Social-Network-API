bac74d13b6fcbeeac58ddfade8fefcdc28dc3507
Simone Anthony <simone.anthony1@yahoo.com>
2022-01-21 22:30:39 -0500

    Though and reaction models modified

---
+++ models/Reaction.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    max: [280, "Provided text must be less than 280 character"],
+  },
+  createdAt: { 
+    type: Date, 
+    default: Date.now,
+    get: v => new Intl.DateTimeFormat('en-US', options).format(v)
+ },
+  username: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = reactionSchema;
--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
+const reactionSchema = require('./Reaction');
 
 const thoughtSchema = new Schema({
   thoughtText: {
@@ -8,14 +9,28 @@ const thoughtSchema = new Schema({
     min: [1, 'Provided text must be more than 1 character'],
     max: [280, 'Provided text must be less than 280 character']
   },
-  createdAt: { type: Date, default: Date.now },
-  
-  // !!!! is a subdocument
-  reactions: [],
-  // thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
-  // friends: [{ type: Schema.Types.ObjectId, ref: "Friends" }],
+  createdAt: { 
+    type: Date, 
+    default: Date.now,
+    get: v => new Intl.DateTimeFormat('en-US', options).format(v)
+   },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [reactionSchema],
+},
+{
+  toJSON: {
+    virtuals: true,
+  },
+  id: false,
 });
-// !!! Needs virtual
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
 const Thought = model("Thought", thoughtSchema);
 
 module.exports = Thought;
