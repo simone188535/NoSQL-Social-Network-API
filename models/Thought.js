a484baec4cc0f0b129e8703f8f0ac6bc6c292b31
Simone Anthony <simone.anthony1@yahoo.com>
2022-01-24 21:54:08 -0500

    createdAt field now show the correct date format

--- models/Thought.js
+++ models/Thought.js
@@ -1,31 +1,43 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
-const reactionSchema = require('./Reaction');
+const reactionSchema = require("./Reaction");
 
-const thoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    min: [1, 'Provided text must be more than 1 character'],
-    max: [280, 'Provided text must be less than 280 character']
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min: [1, "Provided text must be more than 1 character"],
+      max: [280, "Provided text must be less than 280 character"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // make this a utility later to reduce redundancy
+      get: (v) =>
+        `${new Intl.DateTimeFormat("en-US", {
+          month: "short",
+          day: "numeric",
+          year: "numeric",
+        }).format(v)} at ${new Intl.DateTimeFormat("en-US", {
+          hour: "numeric",
+          minute: "numeric",
+        }).format(v)}`,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
-  createdAt: { 
-    type: Date, 
-    default: Date.now,
-    get: v => new Intl.DateTimeFormat('en-US', {options}).format(v)
-   },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: [reactionSchema],
-},
-{
-  toJSON: {
-    virtuals: true,
-  },
-  id: false,
-});
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 thoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length;
